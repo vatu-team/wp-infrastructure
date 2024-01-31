be05e1c06d855f2e33bcd6cb28a86c2309db6e3c
Michael Bragg <michael@michaelbragg.com>
2024-01-31 21:28:27 +0000

    Added: Abstraction for base of all WordPress.

---
+++ src/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Main Abstraction.
+ *
+ * @package   ThoughtsIdeas\WordPress\Infrastructure
+ * @author    Thoughts & Ideas <hello@thoughtsandideas.uk>
+ * @link      https://www.thoughtsandideas.uk/
+ * @license   MIT
+ * @copyright 2022-2024 Thoughts & Ideas Limited.
+ */
+
+declare(strict_types=1);
+
+namespace ThoughtsIdeas\WordPress\Infrastructure;
+
+abstract class Main
+{
+}
