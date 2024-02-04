f64a8e3f8ab200658549b35eda1214606157eee3
Michael Bragg <michael@michaelbragg.com>
2024-02-04 21:03:34 +0000

    Added: Service contracts

---
+++ src/Services/Registerable.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Interface: Registerable
+ *
+ * Register class with WordPress.
+ *
+ * @package   ThoughtsIdeas\WordPress\Infrastructure
+ * @author    Thoughts & Ideas <hello@thoughtsandideas.uk>
+ * @link      https://www.thoughtsandideas.uk/
+ * @license   MIT
+ * @copyright 2022-2024 Thoughts & Ideas Limited.
+ */
+
+declare(strict_types=1);
+
+namespace ThoughtsIdeas\WordPress\Infrastructure\Services;
+
+interface Registerable
+{
+    /**
+     * Register the service.
+     *
+     * @return void
+     */
+    public function register(): void;
+}
---
+++ src/Services/Service.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Interface: Service
+ *
+ * @package   ThoughtsIdeas\WordPress\Infrastructure
+ * @author    Thoughts & Ideas <hello@thoughtsandideas.uk>
+ * @link      https://www.thoughtsandideas.uk/
+ * @license   MIT
+ * @copyright 2022-2024 Thoughts & Ideas Limited.
+ */
+
+ declare(strict_types=1);
+
+ namespace ThoughtsIdeas\WordPress\Infrastructure\Services;
+
+interface Service
+{
+}
